5b431dc9f6e7273c07ad0c7e89da562cf724099e
Priya Sundaresan <priya.sundaresan@berkeley.edu>
2020-07-08 11:31:44 -0700

    Documented a little bit

--- train.py
+++ train.py
@@ -10,22 +10,27 @@ from keras.optimizers import Adam
 from keras.models import Model
 from dataset import DataGenerator
 
+# NOTE: batch_size > 1 does not work, errors at line:
+# class1 = Dense(1024, activation='relu')(flat1)
+# Because of this, we cannot use huge datasets yet, I recommend < 1000 image/action pairs
+#if you find a fix lmk, think we need to add input_dims argument or something...
 params = {"batch_size":1,
-	  "action_dim":6,
+	  "action_dim":6, # (pull_x, pull_y, drop_x, drop_y, hold_x, hold_y)
           "img_dim": (640,480,3)}
-dataset_name = 'chord_single_knot'
+# It will look for images at /datasets/chord_single_knot/images and actions at  /datasets/chord_single_knot/actions
+dataset_name = 'chord_single_knot' 
 train_generator = DataGenerator(dataset_name, **params)
-nb_epoch = 1
+nb_epoch = 100 # Train for 100 epochs
 
 model = VGG16(include_top=False, input_shape=params["img_dim"])
 flat1 = Flatten()(model.outputs)
-class1 = Dense(1024, activation='relu')(flat1)
+class1 = Dense(1024, activation='relu')(flat1) # This is the line that will error for batch_size > 1
 output = Dense(params["action_dim"], activation='linear')(class1)
 model = Model(inputs=model.inputs, outputs=output)
 
 model.compile(loss='mean_squared_error',
               optimizer=Adam(lr=1e-4),
-              metrics=['mean_squared_error'])
+              metrics=['mean_squared_error']) # Using L2 loss for regression
 
 model.fit_generator(generator=train_generator, nb_epoch=nb_epoch)
-model.save("bc_model")
+model.save("bc_model") # Note, only saving at the end of training currently
